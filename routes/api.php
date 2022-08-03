f25706a359fdfe4e57482af0d61e728f9f5d34cc
dryelixir95 <dryelixir95@gmail.com>
2022-08-03 22:38:46 +0700

    tambah auth regis dan route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@ php <?php
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\API\Auth\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('register', 'api\Auth\AuthController@register');
